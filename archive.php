34edaaaf3272d76f00a1a6c2cba98a244ed8d377
Eisuke <eo.710211@gmail.com>
2022-06-30 17:02:16 +0900

    全範囲CSS修正完了

--- archive.php
+++ archive.php
@@ -1,29 +1,30 @@
-    <?php get_header(); ?>
+<?php get_header(); ?>
 
-     <article class="l-article p-article">
+    <article class="l-article p-article">
 
-        <div class="p-archive-visual">
-          <div class="p-archive-visual__box">
-            <h2 class="p-archive-visual__title c-title">Menu:</h2><p class="p-archive-visual__text"><?php single_cat_title(); ?></p>
-          </div>
+      <div class="p-archive-visual">
+        <div class="p-archive-visual__box">
+          <h2 class="p-archive-visual__title c-title">
+            Menu:<p class="p-archive-visual__text"><?php single_cat_title(); ?></p>
+          </h2>
         </div>
+      </div>
 
-        <div class="p-archive-wrapper">
+      <div class="p-archive-wrapper">
 
-          <div class="p-subheading">
-            <?php echo category_description(); ?>
-          </div>
+        <div class="p-subheading">
+          <?php echo category_description(); ?>
+        </div>
 
           <?php get_template_part('components/archive'); ?>
 
-        </div>
+      </div>
 
-        <?php wp_pagenavi(); ?>
+      <?php wp_pagenavi(); ?>
 
-      </article>
+    </article>
 
-    </main>
+  </main>
 
-    <?php get_sidebar(); ?> 
-
-    <?php get_footer(); ?>
+<?php get_sidebar(); ?> 
+<?php get_footer(); ?>
